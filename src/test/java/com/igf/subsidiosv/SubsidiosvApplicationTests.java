60aa4bfd2f94ec7d7945d8063ccce3ba21118ddd
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-10-24 23:28:16 -0600

    Agregando Proyecto Spring

---
+++ src/test/java/com/igf/subsidiosv/SubsidiosvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igf.subsidiosv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsidiosvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
