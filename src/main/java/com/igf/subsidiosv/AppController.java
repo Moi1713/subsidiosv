8c4b22798bcde0f9ed24cd5307e63cb8d34fad02
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-11-01 22:56:25 -0600

    Modificando paginas

--- src/main/java/com/igf/subsidiosv/AppController.java
+++ src/main/java/com/igf/subsidiosv/AppController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/index"})
     public String index(){
         return "index";
     }
