d028eaeddf918a3d7b0409749f74b52a8aec1a8d
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-11-03 23:10:49 -0600

    Creacion de Roles

--- src/main/java/com/igf/subsidiosv/rol/RolController.java
+++ src/main/java/com/igf/subsidiosv/rol/RolController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RolController {
@@ -20,9 +21,15 @@ public class RolController {
         return "roles/roles";
     }
 
-    @GetMapping("/roles/nuevo")
-    public String nuevoRol(Model model) {
+    @GetMapping("/rolnuevo")
+    public String mostrarFormularioDeNuevoRol(Model model) {
         model.addAttribute("rol", new Rol());
-        return "rol_form";
+        return "roles/rol_form";
+    }
+
+    @PostMapping("/roles/guardar")
+    public String gusrdarRol(Rol rol) {
+        rolRepository.save(rol);
+        return "redirect:/roles";
     }
 }
