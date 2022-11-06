83cce37604dd0d396693565f9fcce1045ae06889
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-11-06 05:05:59 -0600

    CRUD completos de Usuarios y Roles

--- src/main/java/com/igf/subsidiosv/usuarios/UsuarioController.java
+++ src/main/java/com/igf/subsidiosv/usuarios/UsuarioController.java
@@ -1,11 +1,13 @@
 package com.igf.subsidiosv.usuarios;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.igf.subsidiosv.rol.Rol;
@@ -41,6 +43,22 @@ public class UsuarioController {
     @PostMapping("/usuarios/guardar")
     public String guardarUsuario(Usuario usuario) {
         usuarioRepository.save(usuario);
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/usuarioeditar/{id}")
+    public String mostrarFormularioDeEditarUsuario(Model model, @PathVariable Integer id) {
+        List<Rol> listaRoles = rolRepository.findAll();
+
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("listaRoles", listaRoles);
+        return "usuarios/usuario_form";
+    }
+
+    @GetMapping("/usuarioeliminar/{id}")
+    public String eliminarUsuario(Model model, @PathVariable Integer id) {
+        usuarioRepository.deleteById(id);
         return "redirect:/usuarios";
     }
 
