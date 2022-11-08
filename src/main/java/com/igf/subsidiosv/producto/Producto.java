c96b572b2f72d9f8dc189405ca9e9638656a4ab0
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-11-07 23:41:46 -0600

    Estructura de proyecto para CRUD

--- src/main/java/com/igf/subsidiosv/producto/Producto.java
+++ src/main/java/com/igf/subsidiosv/producto/Producto.java
@@ -1,5 +1,11 @@
 package com.igf.subsidiosv.producto;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Producto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
