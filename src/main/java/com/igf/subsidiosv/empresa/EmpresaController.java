91c139ab4d2b5715d8cd9204c0b4d4ded9148658
Aleman0604 <113564225+Aleman0604@users.noreply.github.com>
2022-11-13 22:15:27 -0600

    ultimas correcciones de CRUB empresa

--- src/main/java/com/igf/subsidiosv/empresa/EmpresaController.java
+++ src/main/java/com/igf/subsidiosv/empresa/EmpresaController.java
@@ -1,16 +1,32 @@
 package com.igf.subsidiosv.empresa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmpresaController {
     
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
     @GetMapping("/empresa")
-    public String empresa(){
+    public String nuevaEmpresa(Model model){
+
+        model.addAttribute("empresa", new Empresa());
 
         return "empresa/empresa";
 
+    }
+
+    @PostMapping("/empresa/guardar")
+    public String guadarEmpresa(Empresa empresa){
+
+        empresaRepository.save(empresa);
+
+        return "redirect:/empresa";
     }
 
 }
