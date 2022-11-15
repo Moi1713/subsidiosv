1d4bc88e21a8f14f39e564d198bb5eca392706c4
Moisés Nelson Flores <37285740+Moi1713@users.noreply.github.com>
2022-11-14 20:06:04 -0600

    Actualizando datos de prueba

--- src/main/resources/scriptsubsidiosv.sql
+++ src/main/resources/scriptsubsidiosv.sql
@@ -14,9 +14,3 @@ INSERT INTO `beneficio` (`id`, `nombre`) VALUES ('1', 'General'), ('2', 'Focaliz
 INSERT INTO `beneficiario` (`id`, `clave`, `direccion`, `dui`, `nombre`, `beneficio_id`) VALUES ('1', '123', 'San Salvador', '123456789', 'Juan Pérez', '2'), ('2', '123', 'Soyapango', '987654321', 'Jose Jose', '1'), ('3', '123', 'Mejicanos', '111122223', 'Margarita del Campo', '2'), ('4', '123', 'San Martin', '222233334', 'Chamba Perez', '1');
 
 INSERT INTO `subsidio` (`id`, `activo`, `canjeos`, `monto`, `beneficio_id`, `producto_id`) VALUES ('1', NULL, '1', '0.85', '1', '3'), ('2', NULL, '1', '5.46', '2', '3'), ('3', NULL, '1', '2.09', '1', '2'), ('4', NULL, '1', '10.13', '2', '2'), ('5', NULL, '1', '2.93', '1', '3'), ('6', NULL, '1', '10.97', '2', '3');
-
-//Insertar en energia
-
-INSERT INTO `empresa` (`id`, `nombre`) VALUES ('1', 'CAESS'), ('2', 'DELSUR'), ('3', 'EEO');
-
-INSERT INTO `consumo` (`id`, `consumo`, `dui`, `empresa_id`, `periodo`) VALUES ('1', '94', '123456789', '1', '2022/09/21 17:27:47'), ('2', '110', '987654321', '2', '2022/11/11 17:27:47'), ('3', '89', '987654321', '2', '2022/10/02 17:27:47'), ('4', '73', '111122223', '1', '2022/11/02 17:27:47'), ('5', '110', '111122223', '2', '2022/10/11 17:27:47'), ('6', '110', '123456789', '1', '2022/09/15 17:27:47');
