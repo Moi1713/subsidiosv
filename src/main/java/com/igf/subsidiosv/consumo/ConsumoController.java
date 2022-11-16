39a36ca6a3fe047dc481d74058d9ec2b45183628
Aleman0604 <113564225+Aleman0604@users.noreply.github.com>
2022-11-16 00:37:45 -0600

    actualizando CRUD consumo

--- src/main/java/com/igf/subsidiosv/consumo/ConsumoController.java
+++ src/main/java/com/igf/subsidiosv/consumo/ConsumoController.java
@@ -3,8 +3,15 @@ package com.igf.subsidiosv.consumo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.igf.subsidiosv.empresa.Empresa;
+import com.igf.subsidiosv.empresa.EmpresaRepository;
+
 import java.util.List;
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 import org.springframework.ui.Model;
 
@@ -13,12 +20,24 @@ public class ConsumoController {
     @Autowired 
     private ConsumoRepository consumoRepository;
 
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
     @GetMapping("/consumo")
-    public String nuevoConsumo(Model model){
-        List<Consumo> listaConsumos = consumoRepository.findAll(); 
+    public String listarConsumo(Model model) {
+        List<Consumo> listaConsumos = consumoRepository.findAll();
         model.addAttribute("listaConsumos", listaConsumos);
 
         return "consumo/consumo";
+    }
+
+    @GetMapping("/consumonuevo")
+    public String mostrarFormularioDeNuevoConsumo(Model model){
+        List<Empresa> listaEmpresas = empresaRepository.findAll(); 
+        model.addAttribute("consumo", new Consumo());
+        model.addAttribute("listaEmpresas", listaEmpresas);
+
+        return "consumo/consumo_form";
 
     }
 
@@ -27,6 +46,21 @@ public class ConsumoController {
         consumoRepository.save(consumo);
         return "redirect:/consumo";
 
+    }
+    @GetMapping("/consumoeditar/{id}")
+    public String mostrarFormularioDeEditarConsumoString(Model model, @PathVariable Integer id) {
+        List<Empresa> listaEmpresas= empresaRepository.findAll();
+
+        Optional<Consumo> consumo = consumoRepository.findById(id);
+        model.addAttribute("consumo", consumo);
+        model.addAttribute("listaEmpresas", listaEmpresas);
+        return "consumo/consumo_form";
+    }
+
+    @GetMapping("/consumoeliminar/{id}")
+    public String eliminarConsumo(Model model, @PathVariable Integer id) {
+        consumoRepository.deleteById(id);
+        return "redirect:/consumo";
     }
 
 }
